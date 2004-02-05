534476c59dfc1074ec3981f771ce4c6ba4ef5766
prescience <prescience@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2004-02-05 21:44:19 +0000

    fixed google search to look at mantisbt.org
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2304 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- top.php
+++ top.php
@@ -25,9 +25,9 @@
 <!-- Search Google -->
 <input class="small" type="text" name="q" size="16" maxlength="255" value="" />&nbsp;
 <input class="small" type="submit" name="sa" value="Search" />&nbsp;
-<input class="small" type="hidden" name="cof" value="L:http://mantisbt.sourceforge.net/images/banner.gif;AH:center;AWFID:124d3521c6d75a4f;" />
-<input class="small" type="hidden" name="domains" value="mantisbt.sourceforge.net" />
-<input class="small" type="hidden" name="sitesearch" value="mantisbt.sourceforge.net" checked="checked" />
+<input class="small" type="hidden" name="cof" value="L:http://www.mantisbt.org/images/banner.gif;AH:center;AWFID:124d3521c6d75a4f;" />
+<input class="small" type="hidden" name="domains" value="www.mantisbt.org" />
+<input class="small" type="hidden" name="sitesearch" value="www.mantisbt.org" checked="checked" />
 <!-- Search Google -->
 </form>
 	</td>
