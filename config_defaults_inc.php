31f54797635410e583b1e2976a710d0b8c3d4c9b
prescience <prescience@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2004-02-14 19:13:31 +0000

    made config_defaults_inc.php work
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2361 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -10,4 +10,13 @@
 	$g_mantis_user_table = 'mantis_user_table';
 
 	$g_mantis_path = 'mantis/';
+
+	########################################
+	# Include user's config, if available
+	########################################
+
+	$t_config_inc = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'config_inc.php';
+	if ( file_exists( $t_config_inc ) ) {
+		require_once( $t_config_inc );
+	}
 ?>
