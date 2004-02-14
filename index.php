6ae85ef0567e9b618ce6a029f0568af19fdd608c
prescience <prescience@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2004-02-14 23:40:04 +0000

    white space fix
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2363 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php include( "top.php" ); ?>
 <?php
-	#include( 'config_defaults_inc.php' );
-
 	function db_connect($p_hostname="localhost", $p_username="root",
 						$p_password="", $p_database="mantis",
 						$p_port=3306 ) {
