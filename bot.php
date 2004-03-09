3f499c57952a3d28ccb4c124b18e579693d757ab
vboctor <vboctor@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2004-03-09 10:09:39 +0000

    Added XHTML Referer Validation link to the bottom of the page.  This should help in making the site XHTML Valid!
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2432 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- bot.php
+++ bot.php
@@ -10,8 +10,25 @@
 		</tr>
 		</table>
 
-		<address>&nbsp;&nbsp;&nbsp;Last modified: <?php echo date( "M d, Y - H:m T", getlastmod() )?></address>
-		<address>&nbsp;&nbsp;&nbsp;Copyright (C) 2000-2004</address>
+		<table width="100%">
+			<tr valign="center">
+				<td>
+					<address>&nbsp;&nbsp;&nbsp;Last modified: <?php echo date( "M d, Y - H:m T", getlastmod() )?></address>
+					<address>&nbsp;&nbsp;&nbsp;Copyright (C) 2000-2004</address>
+				</td>
+				<td>
+					<p align="right">
+						<a href="http://validator.w3.org/check/referer">Check XHTML Validity</a>
+					</p>
+
+<!-- Add this logo when the site is XHTML compatible
+					<a href="http://validator.w3.org/check/referer"><img border="0"
+						src="http://www.w3.org/Icons/valid-xhtml10" height="31" width="88"
+						alt="Valid XHTML 1.0!" /></a>
+-->
+				</td>
+			</tr>
+		</table>
 	</td>
 </tr>
 </table>
