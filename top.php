0f4820155d881722680a18ae0c7a09a8f49bf47d
vboctor <vboctor@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2004-09-02 13:03:27 +0000

    Updated the search functionality in the website.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2923 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- top.php
+++ top.php
@@ -23,12 +23,25 @@
 <tr class="top">
 	<td class="middle" colspan="3">
 <form method="GET" action="http://www.google.com/search">
-<!-- Search Google -->
-<input class="small" type="text" name="q" size="16" maxlength="255" value="" />&nbsp;
-<input class="small" type="submit" name="btnG" value="Search" />&nbsp;
-<input class="small" type="hidden" name="domains" value="www.mantisbt.org" />
-<input class="small" type="hidden" name="sitesearch" value="www.mantisbt.org" checked="checked" />
-<!-- Search Google -->
+<!-- SiteSearch Google -->
+<form method="get" action="http://www.google.com/custom" target="_top">
+<input type="hidden" name="domains" value="mantisbt.org"></input>
+<input type="radio" name="sitesearch" value=""></input>
+<font size="-1" color="#000000">Web</font>
+<input type="radio" name="sitesearch" value="mantisbt.org" checked="checked"></input>
+<font size="-1" color="#000000">mantisbt.org</font>
+&nbsp;
+<input type="text" name="q" size="25" maxlength="255" value=""></input>
+<input type="submit" name="sa" value="Search"></input>
+<input type="hidden" name="client" value="pub-0979518001849295"></input>
+<input type="hidden" name="forid" value="1"></input>
+<input type="hidden" name="channel" value="0813631605"></input>
+<input type="hidden" name="ie" value="ISO-8859-1"></input>
+<input type="hidden" name="oe" value="ISO-8859-1"></input>
+<input type="hidden" name="cof" value="GALT:#008000;GL:1;DIV:#336699;VLC:663399;AH:center;BGC:FFFFFF;LBGC:336699;ALC:0000FF;LC:0000FF;T:000000;GFNT:0000FF;GIMP:0000FF;FORID:1;"></input>
+<input type="hidden" name="hl" value="en"></input>
+</form>
+<!-- SiteSearch Google -->
 </form>
 	</td>
 </tr>
