1ce6f4b6e9855849ce93a0fae47b31a3b9a16108
vboctor <vboctor@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2004-09-08 13:36:18 +0000

    - Added a link to Bugzilla to Mantis migration script by Cris Daniluk. - Changed all links to open in a blank window.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2938 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- links.php
+++ links.php
@@ -3,71 +3,76 @@
 <span class="page_title">Links</span>
 <hr size=1 noshade width="100%">
 
-<p>Links to work which integrates Mantis into...</p>
+<p><b>Links to work which integrates Mantis into...</b></p>
 <ul>
-<li><a href="http://dev.xoops.org/modules/xfmod/project/?mantis">XOOPS</a></li>
-<li><a href="http://lottasophie.sourceforge.net/modules.php?op=modload&name=Sections&file=index&req=viewarticle&artid=15&page=1">Postnuke</a></li>
-<li><a href="http://www.phprojekt.com/modules.php?op=modload&name=Downloads&file=index&req=viewdownloaddetails&lid=69&ttitle=Mantis_(bugtracker)_as_addon">PHProject</a></li>
-<li><a href="http://sourceforge.net/project/showfiles.php?group_id=68255&package_id=108965">Geeklog</a></li>
+<li><a href="http://dev.xoops.org/modules/xfmod/project/?mantis" target="_blank">XOOPS</a></li>
+<li><a href="http://lottasophie.sourceforge.net/modules.php?op=modload&name=Sections&file=index&req=viewarticle&artid=15&page=1" target="_blank">Postnuke</a></li>
+<li><a href="http://www.phprojekt.com/modules.php?op=modload&name=Downloads&file=index&req=viewdownloaddetails&lid=69&ttitle=Mantis_(bugtracker)_as_addon" target="_blank">PHProject</a></li>
+<li><a href="http://sourceforge.net/project/showfiles.php?group_id=68255&package_id=108965" target="_blank">Geeklog</a></li>
+</ul>
+
+<p><b>Migration Tools</b></p>
+<ul>
+<li>Bugzilla to Mantis (<a href="http://crisdaniluk.com/bugzilla2mantis/bugzilla2mantis.pl" target="_blank">Perl script</a>) by <a href="http://crisdaniluk.com/" target="_blank">Cris Daniluk</a></li>
 </ul>
 
 <br/>
-<p>These links are software packages that are related to Mantis use and development.</p>
+<p><b>These links are software packages that are related to Mantis use and development.</b></p>
 <table width=100%>
 <tr valign="top">
 	<td>
 	<span class="section">Essentials</span>
 	<ul>
-		<li><a href="http://www.php.net">PHP</a>
-		<li><a href="http://www.apache.org">Apache</a>
-		<li><a href="http://www.mysql.com">MySQL</a>
+		<li><a href="http://www.php.net" target="_blank">PHP</a>
+		<li><a href="http://www.apache.org" target="_blank">Apache</a>
+		<li><a href="http://www.mysql.com" target="_blank">MySQL</a>
 	</ul>
 	<span class="section">Helpful</span>
 	<ul>
-		<li><a href="http://www.phpwizard.net/">phpMyAdmin</a>
+		<li><a href="http://www.phpwizard.net/" target="_blank">phpMyAdmin</a>
 	</ul>
 	<span class="section">Other</span>
 	<ul>
-		<li><a href="http://www.aditus.nu/jpgraph/">JpGraph</a>
-		<li><a href="http://phpmailer.sourceforge.net/">phpmailer</a>
-		<li><a href="http://www.phorum.org/">phorum</a>
-		<li><a href="http://www.phpbb.com/">phpbb</a>
+		<li><a href="http://www.aditus.nu/jpgraph/" target="_blank">JpGraph</a>
+		<li><a href="http://phpmailer.sourceforge.net/" target="_blank">PHPMailer</a>
+		<li><a href="http://www.phorum.org/" target="_blank">phorum</a>
+		<li><a href="http://www.phpbb.com/" target="_blank">phpbb</a>
 	</ul>
 	<span class="section">Sites</span>
 	<ul>
-		<li><a href="http://www.sourceforge.net/">SourceForge</a>
-		<li><a href="http://www.freshmeat.net/">Freshmeat</a>
-		<li><a href="http://www.hotscripts.com/">Hot Scripts</a>
-		<li><a href="http://www.phpbuilder.net/">PHPBuilder</a>
-		<li><a href="http://www.webmonkey.com/">Web Monkey</a>
-		<li><a href="http://www.devshed.com/">DevShed</a>
-		<li><a href="http://www.w3c.org/">W3C</a>
+		<li><a href="http://www.sourceforge.net/" target="_blank">SourceForge</a>
+		<li><a href="http://www.freshmeat.net/" target="_blank">Freshmeat</a>
+		<li><a href="http://www.hotscripts.com/" target="_blank">Hot Scripts</a>
+		<li><a href="http://www.phpbuilder.net/" target="_blank">PHPBuilder</a>
+		<li><a href="http://www.webmonkey.com/" target="_blank">Web Monkey</a>
+		<li><a href="http://www.devshed.com/" target="_blank">DevShed</a>
+		<li><a href="http://www.w3c.org/" target="_blank">W3C</a>
 	</ul>
 	</td>
 	<td>
 	<span class="section">Docs</span>
 	<ul>
-		<li><a href="http://www.php.net/manual/en/">PHP Manual</a>
-		<li><a href="http://www.mysql.com/doc/">MySQL Documantation</a>
-		<li><a href="http://utvikler.start.no/code/php_coding_standard.html">PHP Coding Standard</a>
-		<li><a href="http://cvsbook.red-bean.com/cvsbook.html">CVS Reference</a>
+		<li><a href="http://www.php.net/manual/en/" target="_blank">PHP Manual</a>
+		<li><a href="http://www.mysql.com/doc/" target="_blank">MySQL Documantation</a>
+		<li><a href="http://utvikler.start.no/code/php_coding_standard.html" target="_blank">PHP Coding Standard</a>
+		<li><a href="http://cvsbook.red-bean.com/cvsbook.html" target="_blank">CVS Reference</a>
 	</ul>
 	<span class="section">Development Tools</span>
 	<ul>
-		<li><a href="http://www.cyclic.com/">CVS</a>
-		<li><a href="http://www.ssh.com/">SSH</a>
-		<li><a href="http://www.openssh.org/">OpenSSH</a>
+		<li><a href="http://www.cyclic.com/" target="_blank">CVS</a>
+		<li><a href="http://www.ssh.com/" target="_blank">SSH</a>
+		<li><a href="http://www.openssh.org/" target="_blank">OpenSSH</a>
 	</ul>
 	<span class="section">Miscellaneous</span>
 	<ul>
-		<li><a href="http://www.badblue.com/">BadBlue</a>
-		<li><a href="http://www.midgard-project.org/">Midgard</a>
+		<li><a href="http://www.badblue.com/" target="_blank">BadBlue</a>
+		<li><a href="http://www.midgard-project.org/" target="_blank">Midgard</a>
 	</ul>
 	<span class="section">Alternatives</span>
 	<ul>
-		<li><a href="http://bugzilla.mozilla.org">Bugzilla</a>
-		<li><a href="http://dcl.sourceforge.net">Double Chocolate Latte</a>
-		<li><a href="http://scarab.tigris.org">Scarab</a>
+		<li><a href="http://bugzilla.mozilla.org" target="_blank">Bugzilla</a>
+		<li><a href="http://dcl.sourceforge.net" target="_blank">Double Chocolate Latte</a>
+		<li><a href="http://scarab.tigris.org" target="_blank">Scarab</a>
 	</ul>
 	</td>
 </tr>
