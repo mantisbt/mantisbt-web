41ab18473048450f521cfefd5e009ba70f393442
prescience <prescience@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2005-02-15 20:30:41 +0000

    Modified string display mechanism to properly display html links
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@3310 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 	}
 	### --------------------
 	function string_display( $p_string ) {
+		$p_string = string_insert_hrefs( $p_string );
 		$p_string = stripslashes( $p_string );
 		$p_string = nl2br( $p_string );
 		return $p_string;
@@ -27,6 +28,56 @@
 					(null, '$t_ip', NOW(), '$t_user_agent', '$t_referer')";
 		mysql_query( $query );
 	}
+	# --------------------
+	# Detect URLs and email addresses in the string and replace them with href anchors
+	function string_insert_hrefs( $p_string ) {
+		$t_change_quotes = false;
+		if( ini_get_bool( 'magic_quotes_sybase' ) ) {
+			$t_change_quotes = true;
+			ini_set( 'magic_quotes_sybase', false );
+		}
+
+		# Find any URL in a string and replace it by a clickable link
+		$p_string = preg_replace( '/(([[:alpha:]][-+.[:alnum:]]*):\/\/(%[[:digit:]A-Fa-f]{2}|[-_.!~*\';\/?%^\\\\:@&={\|}+$#\(\),\[\][:alnum:]])+)/se',
+                                                                 "'<a href=\"'.rtrim('\\1','.').'\">\\1</a> [<a href=\"'.rtrim('\\1','.').'\" target=\"blank\">^</a>]'",
+                                                                 $p_string);
+		if( $t_change_quotes ) {
+			ini_set( 'magic_quotes_sybase', true );
+		}
+
+		# Set up a simple subset of RFC 822 email address parsing
+		#  We don't allow domain literals or quoted strings
+		#  We also don't allow the & character in domains even though the RFC
+		#  appears to do so.  This was to prevent &gt; etc from being included.
+		#  Note: we could use email_get_rfc822_regex() but it doesn't work well
+		#  when applied to data that has already had entities inserted.
+		#
+		# bpfennig: '@' doesn't accepted anymore
+		$t_atom = '[^\'@\'](?:[^()<>@,;:\\\".\[\]\000-\037\177 &]+)';
+
+		# In order to avoid selecting URLs containing @ characters as email
+		#  addresses we limit our selection to addresses that are preceded by:
+		#  * the beginning of the string
+		#  * a &lt; entity (allowing '<foo@bar.baz>')
+		#  * whitespace
+		#  * a : (allowing 'send email to:foo@bar.baz')
+		#  * a \n, \r, or > (because newlines have been replaced with <br />
+		#    and > isn't valid in URLs anyway
+		#
+		# At the end of the string we allow the opposite:
+		#  * the end of the string
+		#  * a &gt; entity
+		#  * whitespace
+		#  * a , character (allowing 'email foo@bar.baz, or ...')
+		#  * a \n, \r, or <
+
+		$p_string = preg_replace( '/(?<=^|&quot;|&lt;|[\s\:\>\n\r])('.$t_atom.'(?:\.'.$t_atom.')*\@'.$t_atom.'(?:\.'.$t_atom.')*)(?=$|&quot;|&gt;|[\s\,\<\n\r])/s',
+								'<a href="mailto:\1" target="_new">\1</a>',
+								$p_string);
+
+		return $p_string;
+	}
+
 
 	db_connect( $g_hostname, $g_db_username, $g_db_password, $g_database_name );
 
