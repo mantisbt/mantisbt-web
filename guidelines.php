16cd5e2d79a06b10126878f6a53b4b46e4cb8b16
prescience <prescience@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2005-07-01 17:42:03 +0000

    updated php coding standards link - with real link this time
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@3650 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- guidelines.php
+++ guidelines.php
@@ -5,7 +5,7 @@
 <p>
 This is the Mantis coding conventions document.  This should assist anyone who wants to modify the code or work on active development.  Anyone who wishes to contribute code <b>must adhere</b> to these guidelines.  <b>Code not following these conventions will be rejected.</b>  If you have suggestions or a good argument for changing some of these guidelines then discuss it on the <a href="mailinglists.php">developer mailing lists</a>.
 <p>
-First, start off by reading the <a href="http://alltasks.net/code/php_coding_standard.html"><b>PHP Coding Standards</b></a> document.  I've deviated in a few places but just about everything in the document applies here as well.
+First, start off by reading the <a href="http://www.phpinfo.net/articles/article_php-coding-standard.html"><b>PHP Coding Standards</b></a> document.  I've deviated in a few places but just about everything in the document applies here as well.
 <p>
 	Above all, write code that is easy to read and maintain.  Comment blocks of code and functions at all times.  And get on my case if I deviate too much as well!
 <p>
