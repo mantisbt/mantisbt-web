bfbb2a0090cb2070e4325305131d22148174924c
vboctor <vboctor@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2005-07-15 10:48:28 +0000

    Added eCoupons to the sponsors page.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@3669 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- sponsors.php
+++ sponsors.php
@@ -6,13 +6,22 @@
 <p>Mantis team is proud to feature the following companies as sponsors of our site.  For information on joining our sponsor group please contact "vboctor at users dot sourceforge dot net".</p>
 
 <table border="0" cellspacing="0" cellpadding="10">
+	<?php /* Sponsor add placed on 23-OCT-2004 */ ?>
 	<tr>
-		<?php /* Sponsor add placed on 23-OCT-2004 */ ?>
-		<td><a href="http://www.capsher.com" target="_blank"><img src="images/sponsors/capsherlogo.jpg" alt="Capsher Logo" width="200" height="55" border="0" /></a></td>
+		<td><a href="http://www.ecoupons.com" target="_blank"><img src="images/sponsors/ecouponslogo.gif" alt="eCoupons Logo" width="187" height="51" border="0" /></a></td>
 		<td>
-			CAPSHER Technology, Inc., founded in 1994, develops PC software for the engineering and scientific industries. With a history of successful engineering applications, we understand the unique types of problems encountered in technical applications.  We have scaleable involvement, and offer flexible integration with the customer's development environment.
+			<a href="http://www.ecoupons.com">eCoupons.com Inc.</a>, founded in 1999, is a leading provider of Online and Printable Coupons.  Currently, eCoupons partners with leading Online Merchants such as Amazon, Best Buy, Buy.com, Dell, Gateway, HP and Office Depot.
 		</td>
 	</tr>
+
+	<?php /* Sponsor add placed on 15-JUL-2005 */ ?>
+	<tr>
+		<td><a href="http://www.capsher.com" target="_blank"><img src="images/sponsors/capsherlogo.jpg" alt="Capsher Logo" width="200" height="55" border="0" /></a></td>
+		<td>
+			<a href="http://www.capsher.com">CAPSHER Technology, Inc.</a>, founded in 1994, develops PC software for the engineering and scientific industries. With a history of successful engineering applications, we understand the unique types of problems encountered in technical applications.  We have scaleable involvement, and offer flexible integration with the customer's development environment.
+		</td>
+	</tr>
+
 </table>
 
 </td>
