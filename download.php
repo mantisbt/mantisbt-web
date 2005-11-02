75056cf26178dba4a03a202018c9d1043c30a74c
vboctor <vboctor@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2005-11-02 12:28:48 +0000

    Added latest stable version to the downloads page.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@3881 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- download.php
+++ download.php
@@ -5,6 +5,8 @@
 <p>
 Mantis is compressed in <span class="code">.tar.gz</span> format which any reasonable decompression program can handle (Unix tar/gunzip, <a href="http://www.winzip.com/">Winzip</a>, <a href="http://www.stuffit.com/">StuffIt</a>).
 <p>
+<p class="center"><a class="bold" href="http://sourceforge.net/project/showfiles.php?group_id=14963&package_id=166159">Download Mantis <?php include("files/VERSION_STABLE") ?></a>
+<p>
 <p class="center"><a class="bold" href="http://sourceforge.net/project/showfiles.php?group_id=14963">Download Mantis <?php include("files/VERSION") ?></a>
 <p>
 The <a href="http://manual.mantisbt.org/manual.installation.upgrading.php">UPGRADING</a> file contains information on upgrading from one version to the next.  Generally, micro releases just require you to copy over files.  Major and minor releases will require you to run the admin_upgrade.php script.  It is always a good idea to back up your database and files before upgrading.
