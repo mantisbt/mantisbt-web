53855aa02fa6e075b873399529181f294cd838ea
vboctor <vboctor@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2006-02-18 02:54:38 +0000

    - Added 19 new companies to Users Directory. - Added the number of users at the bottom of the page.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@3988 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- directory.php
+++ directory.php
@@ -1,12 +1,18 @@
 <?php include( "top.php" ); ?>
 
 <?php
+	$g_entry_count = 0;
+
 	function section( $p_letter ) {
 		echo "<p><a name=\"$p_letter\" id=\"$p_letter\" /><span class=\"section\">- $p_letter -</span></p>\n";
 		echo "<ul>\n";
 	}
 
 	function entry( $p_name, $p_url, $p_mantis_url = null ) {
+		global $g_entry_count;
+
+		$g_entry_count++;
+
 		if ( null === $p_mantis_url ) {
 			echo "<li><a href=\"$p_url\" target=\"_blank\">$p_name</a></li>\n";
 		} else {
@@ -31,6 +37,12 @@ please download it and reference it from your site to reduce the load on the Man
 
 <?php
 	section( 'A' );
+		entry( 'AG SOFTWARE', 'http://www.agsoftware.hu/' );
+		entry( 'aitools.org', 'http://aitools.org/' );
+		entry( 'AlphaCarina Software GmbH', 'http://www.alphacarina.de/' );
+		entry( 'Applied Informatics', 'http://www.appinf.com/' );
+		entry( 'Asterisk Project', 'http://www.digium.com/', 'http://bugs.digium.com/' );
+		entry( 'Avenity', 'http://www.avenity.com/' );
 		entry( 'Axon Pro', 'http://www.axonpro.sk/', null );
 		entry( 'AYS Systems', 'http://www.ayssystems.co.uk/', null );
 	section_end();
@@ -47,8 +59,10 @@ please download it and reference it from your site to reduce the load on the Man
 		entry( 'CAPSHER Technology, Inc.', 'http://www.capsher.com/', null );
 		entry( 'Cel Kunststoffen', 'http://celkunststoffen.khlim.be/cel/', null );
 		entry( 'chan_sccp', 'http://chan-sccp.sourceforge.net/', 'http://www.czmok.de/devtrack/' );
+		entry( 'Circuitlink International', 'http://www.circuitlink.com.au/' );
 		entry( 'clarkconnect', 'http://www.clarkconnect.org/', 'http://www.clarkconnect.org/bugs/' );
 		entry( 'CMSform<sup>E</sup>', 'http://cmsforme.sourceforge.net/', 'http://cmsforme.sourceforge.net/bugs/' );
+		entry( 'Codestuff LTD', 'http://www.codestuff.net/' );
 		entry( 'Control Engineering Group', 'http://control.ee.ethz.ch/~ceg/', 'http://control.ee.ethz.ch/~ceg/mantis/' );
 		entry( 'Convergys', 'http://www.convergys.com/', null );
 		entry( 'csUnit', 'http://www.csunit.org/', null );
@@ -65,6 +79,7 @@ please download it and reference it from your site to reduce the load on the Man
 
 	section( 'E' );
 		entry( 'Earth Systems Science Computational Centre (ESSCC)', 'http://www.esscc.uq.edu.au' );
+		entry( 'Easy To Work', 'http://www.easytowork.nl/' );
 		entry( 'eboxy', 'http://eboxy.sourceforge.net/', 'http://eboxy.sourceforge.net/mantis/' );
 		entry( 'Edoceo, Inc.', 'http://www.edoceo.com/', null );
 		entry( 'encoway', 'http://www.encoway.de/', null );
@@ -74,34 +89,46 @@ please download it and reference it from your site to reduce the load on the Man
 	section( 'F' );
 		entry( 'Forum One Communications', 'http://www.forumone.com/' );
 		entry( 'Fraunhofer IPSI', 'http://ipsi.fraunhofer.de/concert/' );
+		entry( 'French ministry of culture and communication, IT department', 'http://www.culture.gouv.fr/' );
 		entry( 'FTPEditor', 'http://www.ftpeditor.com/', 'http://www.ftpeditor.com/support/mantis/' );
 	section_end();
 
 	section( 'G' );
 		entry( 'Gambit Consulting', 'http://www.gambit.de/' );
+		entry( 'GROU.PS', 'http://grou.ps/' );
 		entry( 'Gruppo Tesi', 'http://www.gruppotesi.com/' );
 	section_end();
 
 	section( 'H' );
+		entry( 'Handydev', 'http://www.handydev.com/' );
 		entry( 'Hitflip', 'http://www.hitflip.de/', null );
 		entry( 'Hostsharing e.G.', 'http://www.hostsharing.net/', 'https://todo.hostsharing.net/' );
 	section_end();
 
 	section( 'I' );
 		entry( 'IDSecuritySuite', 'http://www.idsecuritysuite.com/' );
+		entry( 'Illumen', 'http://www.illumen.co.uk/' );
 		entry( 'Intra2net', 'http://www.intra2net.com/' );
 		entry( 'Institute for Computational Biomedicine (ICB)', 'http://icb.med.cornell.edu/', 'http://icbtools.med.cornell.edu/mantis/' );
+		entry( 'inveress.net', 'http://www.inveress.net/' );
 		entry( 'I-Tul Design & Software', 'http://www.i-tul.com/' );
 	section_end();
 
 	section( 'J' );
 		entry( 'Jack Audio Connection Kit', 'http://jackit.sourceforge.net/', 'http://jackit.sourceforge.net/mantis/' );
+		entry( 'JavlinConsulting', 'http://www.javlinconsulting.com/' );
 		entry( 'julitec', 'http://www.julitec.de/' );
+	section_end();
+
+	section( 'K' );
+		entry( 'Knowmax', 'http://www.knowmax.nl/', 'http://www.caremax.nl/' );
 	section_end();
 
 	section( 'L' );
 		entry( 'LanAction', 'http://lanaction.sourceforge.net/', 'http://lanaction.sourceforge.net/bugtracker/' );
+		entry( 'LifeType', 'http://www.lifetype.de/', 'http://bugs.lifetype.net/' );
 		entry( 'LIVEwwware', 'http://www.livewwware.com/', 'http://mantis.livewwware.com/' );
+		entry( 'Logical Arts', 'http://www.logicalartsinc.com/' );
 	section_end();
 
 	section( 'M' );
@@ -111,6 +138,7 @@ please download it and reference it from your site to reduce the load on the Man
 
 	section( 'N' );
 		entry( 'National Centre for Applied Learning Technologies (NCALT)', 'http://www.ncalt.com' );
+		entry( 'Navigo Systems', 'http://www.navigo.com.au/' );
 		entry( 'Nox Wizard', 'http://noxwizard.sourceforge.net/', 'http://noxwizard.sourceforge.net/mantis/' );
 	section_end();
 
@@ -141,10 +169,12 @@ please download it and reference it from your site to reduce the load on the Man
 		entry( 'RageIRCd', 'http://rageircd.sourceforge.net/', 'http://rageircd.sourceforge.net/mantis/' );
 		entry( 'RealDecoy', 'http://www.realdecoy.com/' );
 		entry( 'Redwood Custom Communications', 'http://www.redwoodcc.com/' );
+		entry( 'redWall Firewall', 'http://www.redwall-firewall.com/', 'http://www.redwall-firewall.com/issue' );
 		entry( 'Reebok Poland S.A.', 'http://www.reebok.pl/', 'http://mantis.reebok.pl/' );
 		entry( 'Risse Design', 'http://risse.org/' );
 		entry( 'Ritlabs', 'http://www.ritlabs.com/', 'http://www.ritlabs.com/bt/' );
 		entry( 'rsbac', 'http://www.rsbac.org/', 'http://bugtracker.rsbac.org/' );
+		entry( 'RS MediaGroup', 'http://www.rsmg.de/' );
 	section_end();
 
 	section( 'S' );
@@ -152,6 +182,7 @@ please download it and reference it from your site to reduce the load on the Man
 		entry( 'Scribus', 'http://www.scribus.net/', 'http://bugs.scribus.net/' );
 		entry( 'SCS Software', 'http://www.scssoft.com' );
 		entry( 'SCTV', 'http://sctv.ucsc.edu/', 'http://sctv.ucsc.edu/mantis/' );
+		entry( 'SELEX SI - Gematronik Weather Radar Systems', 'http://www.gematronik.com/' );
 		entry( 'Shadow', 'https://sourceforge.net/projects/shadow/', 'http://www.open-shadow.org/mantis/' );
 		entry( 'six apart', 'http://www.sixapart.com/', 'http://sixapart.com/mantis/' );
 		entry( 'SkyGate International', 'http://www.skygate.dk/' );
@@ -170,6 +201,7 @@ please download it and reference it from your site to reduce the load on the Man
 
 	section( 'U' );
 		entry( 'Uncommonplace', 'http://www.uncommonplace.com/', 'http://www.uncommonplace.com/bugs/' );
+		entry( 'United Water International', 'http://www.uwi.com.au/' );
 		entry( 'UnrealIRCd', 'http://www.unrealircd.org/', 'http://bugs.unrealircd.org/' );
 		entry( 'USA Z&amp;P', 'http://usazyp.com.ve/' );
 	section_end();
@@ -183,8 +215,25 @@ please download it and reference it from your site to reduce the load on the Man
 		entry( 'Wash One', 'http://www.wash-one.com/' );
 		entry( 'Water Corporation', 'http://www.watercorporation.com.au/' );
 		entry( 'Web-CP', 'http://www.web-cp.net/', 'http://www.web-cp.net/mantis/' );
+		entry( 'WebCommons :: Web Programming Services', 'http://www.webcommons.biz/' );
+		entry( 'wiseGEEK', 'http://www.wisegeek.com/' );
 		entry( 'WordPress', 'http://wordpress.org/', 'http://mosquito.wordpress.org/' );
 	section_end();
+
+	section( 'X' );
+		entry( 'Xillion ICT Solutions BV', 'http://www.xillion.nl/' );
+		entry( 'XSALTO', 'http://www.xsalto.com/' );
+	section_end();
+
+	section( 'Y' );
+		entry( 'Yoper', 'http://www.yoper.com/', 'http://www.yoper.com/bugtracker/' );
+	section_end();
+
+	section( 'Z' );
+		entry( 'Zoobox', 'http://www.zoobox.co.uk/' );
+	section_end();
+
+	echo '<p>', $g_entry_count, ' members in directory.</p>';
 ?>
 
 <?php include( "adsense_vertical_inc.php" ); ?>
