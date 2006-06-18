36ddc20ccf6e88640b43a10dbbb6f48309b109b4
ryandesign <ryandesign@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2006-06-18 13:37:55 +0000

    Fix bug #7177: The link to the SourceForge ViewVC installation changed.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@4108 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- cvs.php
+++ cvs.php
@@ -7,7 +7,7 @@ Mantis uses <a href="http://www.cyclic.com/">CVS</a> to store and control its so
 Follow the link and follow the commands there.  The module name you want to use is <i>mantisbt</i>.
 <p>
 <p class="center"><a class="bold" href="http://sourceforge.net/cvs/?group_id=14963">Mantis CVS</a>
-<p class="center"><a class="bold" href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/mantisbt">Browse Mantis CVS</a>
+<p class="center"><a class="bold" href="http://mantisbt.cvs.sourceforge.net/mantisbt/">Browse Mantis CVS</a>
 <p class="center"><a class="bold" href="http://cvs.sourceforge.net/cvstarballs/mantisbt-cvsroot.tar.bz2">Download Nightly Tarball</a>
 
 <?php include( "adsense_vertical_inc.php" ); ?>
