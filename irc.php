8b744b413cc3ea8e438e897a712f2ce6156bb774
vboctor <vboctor@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2006-08-06 04:08:26 +0000

    Fixed #6670: Incorrect Freenode IRC Network address.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@4123 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- irc.php
+++ irc.php
@@ -3,7 +3,7 @@
 <span class="page_title">IRC</span>
 <hr size="1" noshade="noshade" width="100%" />
 <p>
-You can find us on IRC in #mantishelp on <a href="http://freenode.info/irc_servers.shtml">Freenode</a> (previously Open Projects Network).
+You can find us on IRC in #mantishelp on <a href="http://freenode.net/irc_servers.shtml">Freenode</a> (previously Open Projects Network).
 <p>
 Idling is a way of life on IRC so do not expect an immediate response.  If you want to be a developer, spending time in IRC is a good way to familiarize yourself with the team.
 <p>
