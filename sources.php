5275ed4d43a8150582badea7a7e818c55359404d
giallu <giallu@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2008-01-02 16:59:02 +0000

    Replace obsolete cvs.php page with a more generic one
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@4854 f5dc347c-c33d-0410-90a0-b07cc1902cb9

---
+++ sources.php
@@ -0,0 +1,24 @@
+<?php
+	$t_sub_title = "Source Control";
+
+	include( "top.php" );
+?>
+
+<span class="page_title">Source Code</span>
+
+<hr size="1" noshade="noshade" width="100%" />
+
+<br />
+
+<span class="section">Subversion (SVN)</span>
+
+<p>Mantis uses <a href="http://subversion.tigris.org/">Subversion</a> to store and control its source code.</p>
+
+<p>Please check the Sourceforge <a href="http://sourceforge.net/svn/?group_id=14963">SVN Repository page</a> for more information about getting Mantis sources.</p>
+
+<p>Look for more documentation about working with Mantis codebase in our <a href="http://www.mantisbt.org/wiki/doku.php/mantisbt:developers_corner">wiki</a></p>
+
+
+<?php include( "adsense_vertical_inc.php" ); ?>
+
+<?php include( "bot.php" ); ?>
