bf73a66a55106bd8661b5c78c3412cb0c1b6fed3
vboctor <vboctor@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2008-04-21 03:12:31 +0000

    Removed irc via cgi-bin and added link to mibbit.com and an embedded widget.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@5180 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- irc.php
+++ irc.php
@@ -9,7 +9,11 @@
 <p>You can find us on IRC in #mantishelp on <a href="http://freenode.net/irc_servers.shtml">Freenode</a> (previously Open Projects Network).</p>
 <p>Idling is a way of life on IRC so do not expect an immediate response.  If you want to be a developer, spending time in IRC is a good way to familiarize yourself with the team.</p>
 <p>For Windows users, we recommend <a href="http://www.silverex.org">X-Chat 2 for Windows</a> as an IRC client.</p>
-<p>If you don't have an IRC client or not familiar with IRC, then you can use your web browser to join the IRC channel by clicking <a href="http://www.mantisbt.org/cgi-bin/irc/irc.cgi">here</a>.</p>
+<p>If you are after a web client, then try widget below or <a href="http://www.mibbit.com/">Mibbit</a>.</p>
+
+<iframe width="600" height="380" scrolling="no" frameborder="0"
+ src="http://embed.mibbit.com/?server=irc.freenode.net&amp;nick=&amp;channel=%23mantishelp">
+</iframe>
 
 <?php include( "adsense_vertical_inc.php" ); ?>
 
