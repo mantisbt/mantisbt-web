9b484170c6d30ff76a955c4e56cdb2c16c6dbabb
vboctor <vboctor@users.sourceforge.net>
2008-11-06 20:11:37 -0800

    Update IRC page to include logs and hyperlink to channel.

--- irc.php
+++ irc.php
@@ -6,15 +6,19 @@
 
 <span class="page_title">IRC</span>
 <hr size="1" noshade="noshade" width="100%" />
-<p>You can find us on IRC in #mantishelp on <a href="http://freenode.net/irc_servers.shtml">Freenode</a> (previously Open Projects Network).</p>
-<p>Idling is a way of life on IRC so do not expect an immediate response.  If you want to be a developer, spending time in IRC is a good way to familiarize yourself with the team.</p>
+<p>MantisBT developers are typically on IRC @ <a href="irc://irc.freenode.net/mantishelp">#mantishelp</a>.
+This IRC channel is hosted on <a href="http://freenode.net/irc_servers.shtml">Freenode</a>.</p>
+
+<p>Archives of IRC channel discussions can be found <a href="http://www.mantisforge.org/irclogs/">here</a>.</p>
+
 <p>For Windows users, we recommend <a href="http://www.silverex.org">X-Chat 2 for Windows</a> as an IRC client.</p>
+
 <p>If you are after a web client, then try widget below or <a href="http://www.mibbit.com/">Mibbit</a>.</p>
 
 <iframe width="600" height="380" scrolling="no" frameborder="0"
  src="http://embed.mibbit.com/?server=irc.freenode.net&amp;nick=&amp;channel=%23mantishelp">
 </iframe>
 
-<?php include( "adsense_vertical_inc.php" ); ?>
-
-<?php include( "bot.php" ); ?>
+<?php
+include( "adsense_vertical_inc.php" );
+include( "bot.php" );
