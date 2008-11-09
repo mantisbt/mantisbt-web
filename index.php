f426a604cefdc0dec4585ab3e25b9cc6a14889b9
Victor Boctor <vboctor@localhost.localdomain>
2008-11-08 16:11:36 -0800

    Move latest versions to configuration.

--- index.php
+++ index.php
@@ -79,8 +79,8 @@
 <tr valign="top">
 	<td class="welcome">
         <p>MantisBT is a free <a href="testimonials.php">popular</a> web-based bugtracking system (<a href="/wiki/doku.php/mantisbt:features">feature list</a>).  It is written in the <a href="http://www.php.net/" rel="nofollow">PHP</a> scripting language and works with <a href="http://www.mysql.com/" rel="nofollow">MySQL</a>, MS SQL, and PostgreSQL databases and a webserver.  MantisBT has been installed on Windows, Linux, Mac OS, OS/2, and others.  Almost any web browser should be able to function as a client.  It is released under the terms of the <a href="http://www.gnu.org/licenses/old-licenses/gpl-2.0.html" rel="nofollow">GNU General Public License</a> (GPL).</p>
-		<p>The latest stable version is <a href="download.php"><?php @include("files/VERSION_STABLE") ?></a>.</p>
-		<p>The latest development version is <a href="download.php"><?php @include("files/VERSION") ?></a>.</p>
+		<p>The latest stable version is <a href="download.php"><?php echo $g_latest_version_stable; ?></a>.</p>
+		<p>The latest development version is <a href="download.php"><?php echo $g_latest_version_dev; ?></a>.</p>
 	</td>
 	<td width="220" align="right">
 	</td>
