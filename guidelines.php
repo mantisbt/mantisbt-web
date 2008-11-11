24a51f2c864ff1e6399ca9fb5552929d41f18639
Sven Lankes <sven@lank.es>
2008-11-11 22:13:09 +0100

    reformat

--- guidelines.php
+++ guidelines.php
@@ -4,8 +4,8 @@
 	include( "top.php" );
 ?>
 
-<span class="page_title">MantisBT Coding Conventions</span>
-<hr size="1" noshade="noshade" width="100%" />
+<h4>MantisBT Coding Conventions</h4>
+
 <p />
 This is the MantisBT coding conventions document.  This should assist anyone who wants to modify the code or work on active development.  Anyone who wishes to contribute code <b>must adhere</b> to these guidelines.  <b>Code not following these conventions will be rejected.</b>  If you have suggestions or a good argument for changing some of these guidelines then discuss it on the <a href="mailinglists.php">developer mailing lists</a>.
 <p />
@@ -13,7 +13,7 @@ First, start off by reading the <a href="http://www.phpinfo.net/articles/article
 <p />
 	Above all, write code that is easy to read and maintain.  Comment blocks of code and functions at all times.  And get on my case if I deviate too much as well!
 <p />
-<span class="section">Naming Variables</span>
+<h5>Naming Variables</h5>
 <ul>
 	<li>Use all lower case.</li>
 	<li>Use _ to separate words, e.g. $green_color_value</li>
@@ -30,7 +30,7 @@ First, start off by reading the <a href="http://www.phpinfo.net/articles/article
 	<li>$query and $result should be used for SQL query and results respectively</li>
 </ul>
 
-<span class="section">Naming Functions</span>
+<h5>Naming Functions</h5>
 <ul>
 	<li>Use all lower case.</li>
 	<li>Use _ to separate words, e.g. setup_page_breaks()</li>
@@ -39,13 +39,13 @@ First, start off by reading the <a href="http://www.phpinfo.net/articles/article
 	<li>Try to use prefixes to group functions (i.e., email_, news_, etc.)</li>
 </ul>
 
-<span class="section">Naming Classes</span>
+<h5>Naming Classes</h5>
 <ul>
 	<li>Use FirstLetterOfWordIsCaptilized style</li>
 	<li>Variables that are class objects should have the prefix coo_</li>
 </ul>
 
-<span class="section">Naming Files</span>
+<h5>Naming Files</h5>
 <ul>
 	<li>Use all lower case.</li>
 	<li>Use _ to separate words, e.g. view_new_bugs_page.php</li>
@@ -54,8 +54,7 @@ First, start off by reading the <a href="http://www.phpinfo.net/articles/article
 	<li>Included files should be suffixed by _inc.php</li>
 </ul>
 
-
-<span class="section">SQL formatting</span>
+<h5>SQL formatting</h5>
 <ul>
 	<li>UPPERCASE all SQL keywords:<br />
 <pre>
@@ -68,7 +67,7 @@ $query = "SELECT *
 	<li>Break up SQL queries over multiple lines to be easy to read.</li>
 </ul>
 
-<span class="section">General Formatting</span>
+<h5>General Formatting</h5>
 <ul>
 	<li>Use TABS with a size of 4</li>
 	<li>Follow the table formatting of existing pages</li>
@@ -77,19 +76,19 @@ $query = "SELECT *
 	<li>Do not use the EOF construct</li>
 </ul>
 
-<span class="section">Miscellaneous</span>
+<h5>Miscellaneous</h5>
 <ul>
 	<li>Don't use the ?: construct except in very rare cases. It is confusing and has a lot of bug potential.</li>
 	<li>Avoid magic numbers.  The only magic numbers in use should be 1 and 0 and their meaning should be obvious.</li>
 </ul>
 
-<span class="section">Page Guidelines</span>
+<h5>Page Guidelines</h5>
 <ul>
 	<li>The first item should be the copyright notice</li>
 	<li>At the bottom will be the footer information and closing print_ functions.</li>
 </ul>
 
-<span class="section">Braces and Parantheses</span>
+<h5>Braces and Parantheses</h5>
 <ul>
 	<li>Parantheses should be right after a function name, e.g. function() not function ()</li>
 	<li>Parantheses should have a space right after a keyword (if, while, for), e.g. for (...)</li>
@@ -118,33 +117,31 @@ if (...) {
     </li>
 </ul>
 
-<span class="section">Comparisons</span>
+<h5>Comparisons</h5>
 <ul>
 	<li>The NOT operator should be placed next to its operand. No spaces, e.g. !$value</li>
 	<li>Parentheses should be used for grouping, especially with multiple comparisons, e.g. if ( ( null == $val ) &amp;&amp; ( null == $val2 ) )</li>
 </ul>
 
-<span class="section">Strings</span>
+<h5>Strings</h5>
 <ul>
 	<li>Use spaces around the string concatenation operator, e.g. 'str ' . $value . ' str2';</li>
 	<li>Use ' instead of " if there are no variables or special characters.</li>
 </ul>
 
-<span class="section">Comments</span>
+<h5>Comments</h5>
 <ul>
 	<li>Use the # symbol for line commenting, not //</li>
 	<li>Use /* */ for block commenting unless you nest /* */ comments.  Generally, only use this during development.</li>
 	<li>Use @@@ followed by a brief message (BROKEN, TEMPORARY, etc) as a "look at this" indicator.  Leaving your name next to is a good idea.  This way critical items can easily be found.</li>
 </ul>
 
-<span class="section">Editor Features</span>
+<h5>Editor Features</h5>
 <ul>
 	<li>Search and replace in multiple files</li>
 	<li>Goto line number</li>
 	<li>Syntax highlighting</li>
 	<li>Adjustable TAB spacing</li>
 </ul>
-
-<?php include( "adsense_vertical_inc.php" ); ?>
 
 <?php include( "bot.php" ); ?>
