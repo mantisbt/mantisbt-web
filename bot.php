1ac8f9c6fa7901a1336f9fbda5847f19ea338491
Sven Lankes <sven@lank.es>
2008-11-11 23:08:49 +0100

    change buttoncontainer layout

--- bot.php
+++ bot.php
@@ -8,23 +8,25 @@ $t_footer_sponsored_links = '';
 ?>
 
 <p />
-
 <div id="buttonContainer">
-
-	<div style="float: left; padding-right: 15px;">
-		<!-- AddThis Bookmark Button BEGIN -->
-		<a href="http://www.addthis.com/bookmark.php" onclick="addthis_url   = location.href; addthis_title = document.title; return addthis_click(this);" target="_blank"><img src="http://s5.addthis.com/button1-bm.gif" width="125" height="16" border="0" alt="AddThis Social Bookmark Button" /></a> <script type="text/javascript">var addthis_pub = 'vboctor';</script><script type="text/javascript" src="http://s5.addthis.com/js/widget.php?v=10"></script>  
-		<!-- AddThis Bookmark Button END -->
-	</div>
-	<div style="float: left; padding-right: 15px;">
-		<?php include('paypal_inc.php'); ?>
-	</div>
-	<div style="float: left; padding-right: 15px;">
-		<img src="images/mantis_logo_button.gif" width="88" height="35" border="0" alt="MantisBT" />
-	</div>
-	<div style="float: left; padding-right: 15px;">
-		<img src="http://sourceforge.net/sflogo.php?group_id=14963" width="88" height="31" border="0" alt="SourceForge Logo" />
-	</div>
+	<table align="center">
+		<tr>
+			<td>
+				<!-- AddThis Bookmark Button BEGIN -->
+				<a href="http://www.addthis.com/bookmark.php" onclick="addthis_url   = location.href; addthis_title = document.title; return addthis_click(this);" target="_blank"><img src="http://s5.addthis.com/button1-bm.gif" width="125" height="16" border="0" alt="AddThis Social Bookmark Button" /></a> <script type="text/javascript">var addthis_pub = 'vboctor';</script><script type="text/javascript" src="http://s5.addthis.com/js/widget.php?v=10"></script>  
+				<!-- AddThis Bookmark Button END -->
+			</td>
+			<td>
+				<?php include('paypal_inc.php'); ?>
+			</td>
+			<td>
+				<img src="images/mantis_logo_button.gif" width="88" height="35" border="0" alt="MantisBT" />
+			</td>
+			<td>
+				<img src="http://sourceforge.net/sflogo.php?group_id=14963" width="88" height="31" border="0" alt="SourceForge Logo" />
+			</td>
+		</tr>
+	</table>
 </div>
 <p />
 <p />
