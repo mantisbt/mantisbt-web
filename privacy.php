0fbbe1015d3a081ff0b51331679b5a65194df2a4
Sven Lankes <sven@lank.es>
2008-11-12 23:21:05 +0100

    fix headings

--- privacy.php
+++ privacy.php
@@ -4,8 +4,7 @@
 	include( "top.php" );
 ?>
 
-<span class="page_title">Privacy Policy</span>
-<hr size="1" noshade="noshade" width="100%" />
+<h4>Privacy Policy</h4>
 
 <p>MantisBT recognizes that privacy is an important privilege that is the right of every user of this website.  To this end, we collect a minimum of personal information.  Personal information that is submitted to MantisBT, including your email address, is held in strict confidence and is never disclosed or sold to any 3rd party organizations.</p>
 
@@ -20,7 +19,7 @@
 
 <p>Users registering in the <a href="forums/">forums</a> may optionally provide other personal information - like instant messenger ids, geographic location, occupation, and so on.  This personal information is publicly viewable.  We are not responsible for any personal information posted to your forum profile or in your forum messages.  Also, if you participate in the <a href="mailinglists.php">mailing lists</a> or post notes to the 'User Contributed Notes' section of the <a href="http://manual.mantisbt.org">manual</a> your email address and signatures will be visible to participants and visitors respectively.</p>
 
-<span class="section">How we use it?</span>
+<h5>How we use it?</h5>
 
 <p>Personal information is primarily used to provide login authentication, personalization and email notifications.  If you have enabled the auto login feature, we use cookies to identify your computer on your return visits.</p>
 
@@ -29,7 +28,7 @@
 <p>We may contact you by email with notifications and other communications that we believe may be relevant to your use of our site.  You may always choose to opt out of receiving such communications.</p>
 
 
-<span class="section">Legal and commercial disclosures</span>
+<h5>Legal and commercial disclosures</h5>
 
 <p>To the extent required by law, we may disclose to the concerned authorities your personal information.  This includes your email address and other personal information available with us.  Information posted to the forums is of course always visible.</p>
 
@@ -41,33 +40,32 @@
 </ul>
 
 
-<span class="section">International data transfers</span>
+<h5>International data transfers</h5>
 
 <p>Information that we collect may be stored and processed in and transferred between any of the countries in which we operate or where our servers are located in order to enable us to use the information in accordance with this privacy policy.</p>
 
 
-<span class="section">Security of your personal data</span>
+<h5>Security of your personal data</h5>
 
 <p>We will take reasonable precautions to prevent the loss, misuse or alteration of your personal information. Of course, data transmission over the internet is inherently insecure, and we cannot guarantee the security of data sent over the internet.</p>
 
 <p>You are responsible for keeping your password confidential. We will not ask you for your password.</p>
 
 
-<span class="section">Policy amendments</span>
+<h5>Policy amendments</h5>
 
 <p>We may update this privacy policy from time-to-time by posting a new version on our website. You should check this page occasionally to ensure you are happy with any changes.  We may also notify you of changes to our privacy policy be email.</p>
 
 
-<span class="section">Third party websites</span>
+<h5>Third party websites</h5>
 
 <p>The website may contains links to other websites. We are not responsible for the privacy policies of third party websites.</p>
 
 
-<span class="section">Contact</span>
+<h5>Contact</h5>
 
 <p>If you have any questions about this privacy policy or our treatment of your personal data, please write to us by email to vboctor at users dot sourceforge dot net.</p>
 
 <p align="center"><small>This privacy policy is based on a freely-available template created and distributed by <a href="http://www.website-law.co.uk" target="_blank" rel="nofollow">http://www.website-law.co.uk</a>.</small></p>
-<?php include( "adsense_vertical_inc.php" ); ?>
 
 <?php include("bot.php"); ?>
