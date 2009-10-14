b0bc08beecb9933d1b75350eef57354f4dacb8e3
Victor Boctor <vboctor@gmail.com>
2009-10-13 22:53:35 -0700

    Update sponsors page.

--- sponsors.php
+++ sponsors.php
@@ -10,9 +10,17 @@
 
 <table border="0" cellspacing="0" cellpadding="10">
 
+	<?php /* Sponsor ad placed on 13-OCT-2009 */ ?>
+	<tr>
+		<td><center><img src="images/sponsors/ladenzeilelogo.png" border="0" alt="Ladenzeile.de" width="200" height="80" /></center></td>
+		<td><a href="http://www.ladenzeile.de/" target="_blank">Ladenzeile.de</a> is a shopping platform focused on fashion, shoes, bags,
+accessories, beauty and furniture. It acts as an aggregator of shops,
+displaying the products of a couple of hundred shops.</td>
+	</tr>
+
 	<?php /* Sponsor ad placed on 28-MAY-2009 */ ?>
 	<tr>
-		<td><center><a href="http://www.rapidrep.com/" target="_blank"><img src="images/sponsors/rapidreplogo.gif" border="0" alt="RapidRep" width="184" height="76" /></a></center></td>
+		<td><center><img src="images/sponsors/rapidreplogo.gif" border="0" alt="RapidRep" width="184" height="76" /></center></td>
 		<td>
 			<a href="http://www.rapidrep.com/" target="_blank">RapidRep</a> Test Suite is a flexible solution for testing arbitrary data via SQL and populating spreadsheet test reports. It focuses on automated test execution and integrates with leading test and defect management systems including MantisBT.
 		</td>
@@ -20,23 +28,15 @@
 
 	<?php /* Sponsor ad placed on 13-DEC-2006 */ ?>
 	<tr>
-		<td><center><a href="http://hitflip.de/" target="_blank"><img src="images/sponsors/hitfliplogo.gif" border="0" alt="Tauschen bei Hitflip" width="135" height="70" /></a></center></td>
+		<td><center><img src="images/sponsors/hitfliplogo.gif" border="0" alt="Tauschen bei Hitflip" width="135" height="70" /></center></td>
 		<td>
 			<a href="http://hitflip.de/" target="_blank">Hitflip</a> is a trading / bartering platform for all sorts of media products like books, music CDs, audiobooks, DVDs / Movies / Videos and games.
 		</td>
 	</tr>
 
-	<?php /* Sponsor ad placed on 05-AUG-2006 */ ?>
-	<tr>
-		<td><center><a href="http://www.absolute-software.co.uk/" target="_blank"><img src="images/sponsors/abssoftlogo.png" border="0" alt="Embedded Programming Services" width="80" height="80" /></a></center></td>
-		<td>
-			<a href="http://www.absolute-software.co.uk/" target="_blank">Absolute Software Ltd</a> provides embedded programming services and PC programming solutions. From simple drivers to complete multiprocessor systems, all on a fixed price basis.
-		</td>
-	</tr>
-
 	<?php /* Sponsor ad placed on 29-AUG-2005 */ ?>
 	<tr>
-		<td><center><a href="http://www.training-classes.com/" target="_blank"><img src="images/sponsors/trainingclasseslogo.gif" border="0" alt="Training" width="200" height="60" /></a></center></td>
+		<td><center><img src="images/sponsors/trainingclasseslogo.gif" border="0" alt="Training" width="200" height="60" /></center></td>
 		<td>
 			<a href="http://www.training-classes.com/" target="_blank">#1 Training Directory</a> Connecting people who want to learn with people who love to teach since 1998: Happy to use MantisBT and proud to support this excellent bug tracking package.
 		</td>
