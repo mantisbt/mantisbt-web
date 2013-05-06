33ecfb54ed5535a6ba31301fadf3b839c246058f
Victor Boctor <vboctor@gmail.com>
2013-05-05 23:22:27 -0700

    Updated consulting page.

--- consulting.php
+++ consulting.php
@@ -5,16 +5,12 @@
 
 <h4>Consulting</h4>
 
-<h5>Installation</h5>
-<p>We offer installation and setup for a fee.</p>
-<p>We will need remote connection and browser access.  This can be accomplished via telnet/ssh, terminal services, VNC, or some other remote access mechanism.</p>
-
-<h5>Customization</h5>
-<p>Our services are available to customize MantisBT to suit your needs.  We design customizations with a special focus on our customers ability to easily upgrade to future versions of MantisBT.  This is done by putting the generic requirements into MantisBT core and by adding extension points that can are used to implement customer specific requirements.</p>
+<p>Our services are available to install, migrate, or customize MantisBT to suit your needs.  We design customizations with a special focus on our customers ability to easily upgrade to future versions of MantisBT.  This is done by putting the generic requirements into MantisBT core and by adding extension points that can are used to implement customer specific requirements.</p>
 <p>We recommend you use MantisBT for a short period to see what changes would best fit your organization.  We may also be available for other web related projects.</p>
 
 <h5>Contacting Us</h5>
 
 <p>Use the <a href="contact_us.php">contact us</a> page to initiate the discussion relating to your consulting needs.</p>
 
-<?php include("bot.php"); ?>
+<?php
+	include("bot.php");
