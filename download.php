f8e3d052ff681f502557528106116f2540ed47cc
Damien Regad <dregad@mantisbt.org>
2013-07-23 10:58:21 +0200

    Download page: CSS and layout adjustments

--- download.php
+++ download.php
@@ -9,40 +9,51 @@
 	Get up and running with <a href="hosting.php">hosted MantisBT</a> in less than 5 minutes for $5/month.
 </div>
 
-<p/>
+<p />
 
-<div style="height: 19em; position: relative;" class="genericbox">
+<div class="genericbox box-fixedheight">
 	<h3>Stable release</h3>
-	This is the latest stable MantisBT release. Download this file for production use.
-	<p align="center">
-		<a class="bold" href="https://sourceforge.net/projects/mantisbt/files/mantis-stable/">Download MantisBT <?php echo $g_latest_version_stable; ?></a><br />
+	<p>This is the latest stable MantisBT release.
+	Please download this file for production use.
 	</p>
-	<p style="position: absolute; top: 17em; left: 3em;">
-		[ <a href="requirements.php">Requirements</a>
-		| <a href="bugs/changelog_page.php?project=mantisbt&version=<?php echo $g_latest_version_stable; ?>">Changelog</a>
-		]
+	<p class="downloadlink">
+		<a href="https://sourceforge.net/projects/mantisbt/files/mantis-stable/">
+			Download MantisBT <?php echo $g_latest_version_stable; ?>
+		</a>
 	</p>
+	<div class="box-bottom">
+		<p class="center">
+			[ <a href="requirements.php">Requirements</a>
+			| <a href="bugs/changelog_page.php?project=mantisbt&version=<?php echo $g_latest_version_stable; ?>">Changelog</a>
+			]
+		</p>
+	</div>
 </div>
 
-<div style="height: 19em; position: relative;" class="genericbox">
+<div class="genericbox box-fixedheight">
 	<h3>Development release</h3>
-	The latest development release is intended for evaluation and testing
-	purposes. It is not recommended for production use, unless you are
-	comfortable with PHP and able to troubleshoot issues that may arise.
-	<p align="center">
-		<a class="bold" href="https://sourceforge.net/projects/mantisbt/files/mantis-development/">Download MantisBT <?php echo $g_latest_version_dev; ?></a>
+	<p>The latest development release is intended for evaluation and testing
+		purposes. It is not recommended for production use, unless you are
+		comfortable with PHP and able to troubleshoot issues that may arise.
 	</p>
-	<p style="position: absolute; top: 17em; left: 3em;">
-		[ <a href="requirements.php">Requirements</a>
-		| <a href="bugs/roadmap_page.php?project=mantisbt&version=<?php echo $g_latest_version_dev; ?>">Roadmap</a>
-		]
+	<p class="downloadlink">
+		<a href="https://sourceforge.net/projects/mantisbt/files/mantis-development/">
+			Download MantisBT <?php echo $g_latest_version_dev; ?>
+		</a>
 	</p>
+	<div class="box-bottom">
+		<p class="center">
+			[ <a href="requirements.php">Requirements</a>
+			| <a href="bugs/roadmap_page.php?project=mantisbt&version=<?php echo $g_latest_version_dev; ?>">Roadmap</a>
+			]
+		</p>
+	</div>
 </div>
 
 <div class="clearBoth"></div>
 
-<div class="genericbox" style="width: 97%">
-	<div style="float: right; margin-right: 1em">
+<div class="genericbox box-doublewidth">
+	<div class="floatRight">
 		<a href="https://travis-ci.org/mantisbt/mantisbt/branches">
 			<img src="https://travis-ci.org/mantisbt/mantisbt.png">
 		</a>
@@ -54,34 +65,34 @@
 	<a href="https://travis-ci.org/mantisbt/mantisbt">Travis CI</a>
 	build process, and have not been formally prepared for release;
 	we recommend you only use them for testing and feedback purposes.
-	<p align="center">
+	<p class="downloadlink">
 		<a class="bold" href="/builds">Download a Nightly Build</a>
 	</p>
 </div>
 
 <div class="clearBoth"></div>
 
-<div class="genericbox" style="width: 97%">
+<div class="genericbox box-doublewidth">
 	<h3>Add-On Software</h3>
 
-	<h4 style="margin-bottom: 0px;">MantisTouch</h4>
-	<div style="margin: 0 20px auto 20px; padding-top: 0px;">
+	<h4 class="addon">MantisTouch</h4>
+	<div class="addon-text">
 		<a href="http://www.mantisbt.org/mantistouch.php">MantisTouch</a> is a web app
-		that provides a mobile optimized MantisBT user interface for iPhone, Android
+		that provides a mobile-optimized MantisBT user interface for iPhone, Android
 		and Windows Phones devices. MantisTouch internally uses web services to access
 		the underlying MantisBT instance, hence providing the mobile friendly
 		interface without requiring any changes for the accessed system.
 	</div>
 
-    <h4 style="margin-bottom: 0px;">MantisConnect</h4>
-	<div style="margin: 0 20px auto 20px; padding-top: 0px;">
+	<h4 class="addon">MantisConnect</h4>
+	<div class="addon-text">
 		<a href="http://www.futureware.biz/mantisconnect/">MantisConnect</a>
 		allows .NET-, Java-, Cocoa- and generally all Applications that can access webservices
 		to integrate with MantisBT.
 	</div>
 
-	<h4 style="margin-bottom: 0px;">MantisBT Related Tools</h4>
-	<div style="margin: 0 20px auto 20px; padding-top: 0px;">
+	<h4 class="addon">MantisBT Related Tools</h4>
+	<div class="addon-text">
 		Check the <a href="/wiki/doku.php/mantisbt:related_tools">related tools</a> page.
 	</div>
 </div>
