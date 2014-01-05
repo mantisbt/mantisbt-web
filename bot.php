138e1a262e2b2decf149be8cb079913fce26d674
Victor Boctor <victor@mantishub.com>
2014-01-05 11:10:01 -0800

    Remove contact us control.

--- bot.php
+++ bot.php
@@ -9,15 +9,5 @@ urchinTracker();
 </script>
 <!-- End of Google Analytics -->
 
-<!--Start of Zopim Live Chat Script-->
-<script type="text/javascript">
-window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
-d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-$.src='//v2.zopim.com/?1YRlJw1kTov68yhki8DJhwacxnM7Bmck';z.t=+new Date;$.
-type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-</script>
-<!--End of Zopim Live Chat Script-->
-
 </body>
 </html>
