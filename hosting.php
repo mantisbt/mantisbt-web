3b01470406772b919bd18b03c07441011be6259f
Victor Boctor <victor@mantishub.com>
2014-01-05 11:28:26 -0800

    Remove references to contact us.

--- hosting.php
+++ hosting.php
@@ -6,7 +6,7 @@
 
 <h4>MantisBT Hosting</h4>
 
-<p>For a hassle-free MantisBT hosting that is accessible to both internal and external users, it's recommended to get a hosted solution with one of the MantisBT certified hosting partners.  Use the <a href="contact_us.php">contact us</a> form for custom installations requests.</p>
+<p>For a hassle-free MantisBT hosting that is accessible to both internal and external users, it's recommended to get a hosted solution with one of the MantisBT certified hosting partners.</p>
 
 <table border="0" cellspacing="0" cellpadding="10">
 
