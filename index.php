d40f51f49d05d7f57fe9c114c08a725c0020476c
Victor Boctor <victor@mantishub.com>
2014-01-05 12:10:26 -0800

    Add MantisBT screenshot to home page.

--- index.php
+++ index.php
@@ -2,5 +2,12 @@
 
 <p>MantisBT is a free popular web-based bugtracking system (<a href="/wiki/doku.php/mantisbt:features">feature list</a>).  It is written in the <a href="http://www.php.net/" rel="nofollow">PHP</a> scripting language and works with <a href="http://www.mysql.com/" rel="nofollow">MySQL</a>, MS SQL, and PostgreSQL databases and a webserver.  MantisBT has been installed on Windows, Linux, Mac OS, OS/2, and others.  Almost any web browser should be able to function as a client.  It is released under the terms of the <a href="http://www.gnu.org/licenses/old-licenses/gpl-2.0.html" rel="nofollow">GNU General Public License</a> (GPL).</p>
 
+<div align="center">
+<img src="images/mantisbt_screenshot.png" width="540" height="308" alt="MantisBT Screenshot" />
+</div>
+
+<br />
+<br />
+
 <?php
 	include_once('bot.php');
