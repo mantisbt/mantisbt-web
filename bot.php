3f959819ad4cf1643bcda09c63dd4e9610a985e6
Victor Boctor <victor@mantishub.com>
2014-01-07 00:19:58 -0800

    Update Google Analytics javascript code to latest version.

--- bot.php
+++ bot.php
@@ -3,11 +3,16 @@
 </div>
 
 <!-- Google Analytics -->
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
 <script type="text/javascript">
-_uacct = "UA-330112-2";
-urchinTracker();
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-330112-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 </script>
 <!-- End of Google Analytics -->
 
