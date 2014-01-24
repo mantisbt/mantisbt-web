cdd984a5706bfb6b1890bb9941d2045f643cb661
Rafik Robeal <rafik@raveable.com>
2014-01-24 13:59:34 -0800

    Complete implementation of the homepage Remove unused images

--- index.php
+++ index.php
@@ -4,158 +4,75 @@
     <div class="bannercontainer">
       <div class="banner">
         <ul>
-          <!-- MAIN CONTENT AREA: SLIDER BANNER (REVOLUTION SLIDER) - SLIDE 1 [SLIDE
-          STYLE=BOXFADE] -->
-          <li class="slide1 slide" data-transition="boxfade" data-slotamount="5"
+          <li class="slide21 slide" data-transition="slotfade-horizontal" data-slotamount="10"
               data-masterspeed="300">
-            <img alt="" src="img/slider01.png" class="img-responsive" />
-            <div class="caption sft carousel-caption" data-x="110" data-y="110" data-speed="1000"
-                 data-start="1000" data-easing="easeInBack" style="background: none;">
-              <p class="middle">Beautiful, clean, ultra-responsive &amp; highly flexible</p>
-            </div>
-            <div class="caption sfl carousel-caption" data-x="98" data-y="141" data-speed="1000"
-                 data-start="1000" data-easing="easeOutBack" style="background: none;">
-              <p class="big">An Incredible New</p>
-            </div>
-            <div class="caption sfl carousel-caption" data-x="98" data-y="191" data-speed="1000"
-                 data-start="1000" data-easing="easeOutBack" style="background: none;">
-              <p class="big">HTML5 Template!</p>
-            </div>
-            <div class="caption sfb carousel-caption" data-x="111" data-y="247" data-speed="1000"
-                 data-start="1000" data-easing="easeInBack" style="background: none; width: 436px;">
-              <p class="small">Bring your website into the 21st century with bizstrap. Beautiful, clean,
-                ultra-responsive and highly flexible bizstrap puts you firmly in control
-                of your new site build or site bizstrap.</p>
-            </div>
-          </li>
-          <!-- MAIN CONTENT AREA: SLIDER BANNER (REVOLUTION SLIDER) - SLIDE 2 [SLIDE
-          STYLE=LEFT] -->
-          <li class="slide2 slide" data-transition="slideleft" data-slotamount="10"
-              data-masterspeed="300">
-            <img alt="" src="img/slider02.png" class="img-responsive" />
+            <img alt="" src="img/slider_3screens.png" class="img-responsive" />
             <div class="caption sft carousel-caption" data-x="472" data-y="69" data-speed="1000"
                  data-start="1000" data-easing="easeInBack" style="background: none;">
-              <p class="middle">Is your business website mobile ready?</p>
+              <p class="middle">What is MantisBT?</p>
             </div>
             <div class="caption sfr carousel-caption" data-x="462" data-y="107" data-speed="1000"
                  data-start="1000" data-easing="easeOutBack" style="background: none;">
-              <p class="big">20% of all web</p>
+              <p class="big">MantisBT is a </p>
             </div>
             <div class="caption sfl carousel-caption" data-x="462" data-y="156" data-speed="1000"
                  data-start="1000" data-easing="easeOutBack" style="background: none;">
-              <p class="big">traffic today is from</p>
+              <p class="big">free, full-featured</p>
             </div>
             <div class="caption sfl carousel-caption" data-x="462" data-y="205" data-speed="1000"
                  data-start="1000" data-easing="easeOutBack" style="background: none;">
-              <p class="big">mobile devices</p>
+              <p class="big">bug & issue tracking system</p>
             </div>
             <div class="caption sfb carousel-caption" data-x="474" data-y="262" data-speed="1000"
                  data-start="1000" data-easing="easeInBack" style="background: none; width: 436px;">
-              <p class="small">The mobile revolution is here. Is your business ready for it? Built using
-                the latest web technology platform and framework; HTML5, CSS3 and Twitter
-                Bootstrap, bizstrap allows you to create one site that renders beautifully
-                on all devices, including computers, tablet and phones.</p>
+              <p class="small">MantisBT is an amazingly simple to install. It is intuitive, you’ll pick it up in
+                seconds or minutes, not hours, days or weeks. MantisBT is an open source bug tracker written in PHP,
+                uses MySQL, and can be installed on Linux, Windows, Mac, and more.</p>
             </div>
           </li>
-          <!-- MAIN CONTENT AREA: SLIDER BANNER (REVOLUTION SLIDER) - SLIDE 3 [SLIDE
-          STYLE=SLIDE DOWN] -->
-          <li class="slide3 slide" data-transition="slidedown" data-slotamount="1"
-              data-masterspeed="300">
-            <img alt="" src="img/slider03.png" class="img-responsive" />
-            <div class="caption sft carousel-caption" data-x="43" data-y="184" data-speed="1000"
-                 data-start="1000" data-easing="easeInBack" style="background: none; padding-left: 0;">
-              <p class="middle">Create beautiful pages quickly and easily...</p>
-            </div>
-            <div class="caption sfl carousel-caption" data-x="0" data-y="228" data-speed="1000"
-                 data-start="1000" data-easing="easeOutBack" style="background: none; padding-left: 0; width: 781px;">
-              <p class="big">Over 30 Shortcuts and Rapid-Design Page Templates!</p>
-            </div>
-            <div class="caption sfb carousel-caption" data-x="0" data-y="281" data-speed="1000"
-                 data-start="1000" data-easing="easeInBack" style="background: none; padding-left: 0; width: 100%;">
-              <p class="small">Creating beautiful and highly functional web pages doesn’t have to take
-                hours. In fact, with over 30 pre-made page templates, bizstrap makes
-                <br
-                  />creating great looking web pages quick and easy.</p>
-            </div>
-          </li>
-          <!-- MAIN CONTENT AREA: SLIDER BANNER (REVOLUTION SLIDER) - SLIDE 4 [SLIDE
-          STYLE=BOXFADE] -->
-          <li class="slide4 slide" data-transition="boxfade" data-slotamount="5"
+          <li class="slide2 slide" data-transition="slideleft" data-slotamount="5"
               data-masterspeed="300">
             <img alt="" src="img/slider07.png" class="img-responsive" />
             <div class="caption sft carousel-caption" data-x="45" data-y="78" data-speed="1000"
                  data-start="1000" data-easing="easeInBack" style="background: none;">
-              <p class="middle">Slap the page with a Sticky Note!</p>
+              <p class="middle">Get help anywhere anytime!</p>
             </div>
             <div class="caption sfl carousel-caption" data-x="26" data-y="107" data-speed="1000"
                  data-start="1000" data-easing="easeOutBack" style="background: none;">
-              <p class="big">Got a special message</p>
+              <p class="big">Active and engaged</p>
             </div>
             <div class="caption sfl carousel-caption" data-x="26" data-y="160" data-speed="1000"
                  data-start="1000" data-easing="easeOutBack" style="background: none;">
-              <p class="big">you want to stand out on a</p>
+              <p class="big">community is the</p>
             </div>
             <div class="caption sfl carousel-caption" data-x="26" data-y="214" data-speed="1000"
                  data-start="1000" data-easing="easeOutBack" style="background: none;">
-              <p class="big">particular web page?</p>
+              <p class="big">backbone of MantisBT</p>
             </div>
             <div class="caption sfb carousel-caption" data-x="42" data-y="265" data-speed="1000"
                  data-start="1000" data-easing="easeInBack" style="background: none; width: 436px;">
-              <p class="small">When you have a special message on a particular web page that you want
-                to get a across to your visitors then slap that page with one of bizstrap's
-                exclusive sticky notes. You'll find that website visitors simply can't
-                resist them.</p>
+              <p class="small">The community around MantisBT is full of nice people who want to help others.
+                Become part of the family, learn from others, and give something back when you can.</p>
             </div>
             <div class="caption sfl carousel-caption stick1" data-x="450" data-y="52"
                  data-speed="1400" data-start="1000" data-easing="easeInExpo" style="background: none;">
-              <img alt="" src="img/slide_stick01.png" class="img-responsive" />
+              <img alt="" src="img/forums.png" class="img-responsive" />
             </div>
             <div class="caption sfl carousel-caption stick2" data-x="607" data-y="52"
                  data-speed="1400" data-start="1500" data-easing="easeInExpo" style="background: none;">
-              <img alt="" src="img/slide_stick02.png" class="img-responsive" />
+              <img alt="" src="img/wiki.png" class="img-responsive" />
             </div>
             <div class="caption sfl carousel-caption stick3" data-x="764" data-y="52"
                  data-speed="1400" data-start="2000" data-easing="easeInExpo" style="background: none;">
-              <img alt="" src="img/slide_stick03.png" class="img-responsive" />
+              <img alt="" src="img/mailing_lists.png" class="img-responsive" />
             </div>
             <div class="caption sfl carousel-caption stick4" data-x="450" data-y="208"
                  data-speed="1400" data-start="2500" data-easing="easeInExpo" style="background: none;">
-              <img alt="" src="img/slide_stick04.png" class="img-responsive" />
+              <img alt="" src="img/bugtracker.png" class="img-responsive" />
             </div>
             <div class="caption sfl carousel-caption stick5" data-x="607" data-y="208"
                  data-speed="1400" data-start="3000" data-easing="easeInExpo" style="background: none;">
-              <img alt="" src="img/slide_stick05.png" class="img-responsive" />
-            </div>
-            <div class="caption sfl carousel-caption stick6" data-x="764" data-y="208"
-                 data-speed="1400" data-start="3500" data-easing="easeInExpo" style="background: none;">
-              <img alt="" src="img/slide_stick06.png" class="img-responsive" />
-            </div>
-          </li>
-          <!-- MAIN CONTENT AREA: SLIDER BANNER (REVOLUTION SLIDER) - SLIDE 5 [SLIDE
-          STYLE=SLOTFADE HORIZONTAL] -->
-          <li class="slide5 slide" data-transition="slotfade-horizontal" data-masterspeed="300"
-              data-slotamount="20">
-            <img alt="" src="img/slider04.png" class="img-responsive" />
-            <div class="caption sft carousel-caption" data-x="468" data-y="71" data-speed="1000"
-                 data-start="1000" data-easing="easeInBack" style="background: none; padding-left: 0;">
-              <p class="middle">Smart, very smart indeed!</p>
-            </div>
-            <div class="caption sfr carousel-caption" data-x="454" data-y="105" data-speed="1000"
-                 data-start="1000" data-easing="easeOutBack" style="background: none;">
-              <p class="big">The Smart Choice for</p>
-            </div>
-            <div class="caption sfr carousel-caption" data-x="454" data-y="154" data-speed="1000"
-                 data-start="1000" data-easing="easeOutBack" style="background: none;">
-              <p class="big">Building Your</p>
-            </div>
-            <div class="caption sfr carousel-caption" data-x="454" data-y="205" data-speed="1000"
-                 data-start="1000" data-easing="easeOutBack" style="background: none;">
-              <p class="big">Business Website</p>
-            </div>
-            <div class="caption sfb carousel-caption" data-x="480" data-y="268" data-speed="1000"
-                 data-start="1000" data-easing="easeInBack" style="background: none; padding-left: 0;">
-              <p class="small">Affordable, effective and easy-to-use bizstrap is the smart choice for
-                building and managing your business website. Smart, very smart indeed!</p>
+              <img alt="" src="img/irc.png" class="img-responsive" />
             </div>
           </li>
         </ul>
@@ -167,30 +84,36 @@
   <div class="main-block">
     <div class="col-sm-12 col-md-12">
       <div class="row">
-        <!-- MAIN CONTENT AREA: REDESIGN CUSTOM - HERO BLOCK 1 (TEXT BLOCK)] -->
+
         <div class="col-sm-6 col-md-6 home-block hero-block-1">
-          <h1>Say hello to bizstrap!</h1>
-          <h2>Clean, modern &amp; ultra-responsive</h2>
-          <p>bizstrap is a clean, modern and ultra-responsive site template designed
-            and built from the ground up using the latest and greatest web technologies
-            and framework. And, with over 30 stunning and highly useful feature shortcuts
-            and rapid-design page templates you can effortlessly turn your organization’s
-            website into a stunning product and/or services showcase that you’ll not
-            only be proud of, but your customers will greatly respect. Affordable,
-            effective and easy-to-use bizstrap is the smart choice for building and
-            managing your business website. Smart, very smart indeed!</p>
+          <h1>Let MantisBT track your bugs!</h1>
+          <h2>Easy, fast &amp; feature complete</h2>
+          <p>
+            MantisBT offers simple and detailed issue pages. It supports multiple projects, subprojects, and
+            different bug types (a.k.a.  categories). It gives individual users roles and privileges, and displays
+            a changelog and roadmap based on relevant bug resolution state. It also allows users to monitor specific
+            issues and organize bugs based on how they relate to a user. It can notify developers and end users of
+            reported bugs through email, RSS feeds, and even Twitter. MantisBT also implements a PHP SOAP library,
+            which allows programs like Eclipse and platforms like Java and .Net to interface with it.
+          </p>
         </div>
-        <!-- MAIN CONTENT AREA: SPACER BETWEEN HERO BLOCK 1 AND HEREO BLOCK 2
-        -->
+
         <div class="col-sm-1 col-md-1"></div>
-        <!-- MAIN CONTENT AREA: bizstrap CUSTOM - HERO BLOCK 2 -->
+
         <div class="col-sm-5 col-md-5 home-block">
           <div class="grey-box hero-block-2">
-            <h2><a href="#">Effective Action Boxes That Get Results</a></h2>
+            <h2><a href="demo.php">Get MantisBT 1.2.15</a></h2>
             <p>
-              <img class="img-responsive" src="img/release_icon.png" alt="">Great web pages utilize effective calls to action to grab the attention
-              of the intended audience. bizstrap allows you to quickly and easily create
-              highly effective action boxes.</p> <a class="btn btn-warning" href="#"><i class="icon-shopping-cart"></i>Buy it now just for $18</a>
+              <img class="img-responsive" src="img/release_icon.png" alt=""/>
+              Download the latest MantisBT release. To try before downloading, click on the demo button.
+              For details on for details on the changes and bug fixes in this release, check out the
+              <a href="http://www.mantisbt.org/bugs/changelog_page.php?version_id=182">changelog</a>.
+            </p>
+
+            <div class="clear-both"></div>
+
+            <a class="btn btn-warning" href="demo.php">Demo</a>
+            <a class="btn btn-warning" href="download.php">Download</a>
           </div>
         </div>
       </div>
@@ -201,7 +124,6 @@
 
   <div class="row main-block index1">
     <div class="col-sm-12 col-md-12">
-      <!-- MAIN CONTENT AREA: bizstrap CUSTOM - HERO LIST -->
       <div class="row show-grid hero-list">
         <div class="col-sm-4 col-md-4">
           <div class="image-wrapper">
@@ -286,7 +208,6 @@
         </div>
       </div>
     </div>
-    <!-- MAIN CONTENT AREA: bizstrap CUSTOM - HERO BLOCK 3 -->
     <div class="col-sm-12 col-md-12 grey-box">
       <div class="hero-block3">
         <div class="row show-grid">
