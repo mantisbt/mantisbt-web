1a1f07b91be91dbbac7074c55e6366cdcd2544bf
Victor Boctor <victor@mantishub.com>
2014-01-24 20:14:48 -0800

    Updated addons page.

--- addons.php
+++ addons.php
@@ -12,64 +12,35 @@ include( "top.php" );
   </div>
 
   <div class="row show-grid clear-both">
-    <div class="col-md-12 col-sm-12">
-      <h1>MantisTouch</h1>
+    <div class="col-sm-7 col-md-8">
+      <h1>MantisTouch<small>| MantisBT Gone Mobile!</small></h1>
       <p>
-      <span class="lead"> <a href="http://www.mantistouch.org">MantisTouch</a> provides a mobile optimized user
-        interface for iPhone, Android, and Windows Phone. </span> MantisTouch is developed and maintained by
-        Victor Boctor. Victor is a member of MantisBT core team and long-time contributor to MantisBT project
-        since 2000. As a web app, MantisTouch uses web services to access the bug tracking data allowing
-        it to be installed on the same or a different server than MantisBT.
+        <span class="lead">
+          MantisTouch provides a mobile optimized user
+          interface for iPhone, Android, and Windows Phone.
+        </span>
+      </p>
+      <p>
+        MantisTouch is developed and maintained by Victor Boctor.
+        Victor is a member of MantisBT core team since 2000.
+        As a web app, MantisTouch provides a mobile optimized experience for all users of your MantisBT instance.
       </p>
     </div>
-  </div>
-
-  <br>
-
-  <div class="row">
-    <div class="col-sm-12 col-md-4 center">
-      <a href="http://www.mantistouch.org">
-        <img class="img-rounded" title="Login Page on Smart Phone" alt="Login Page" src="images/mt_login_page.png" height="300" width="200" border="1" />
-      </a>
-      <div class="clearfix"></div>
-      <br>
-    </div>
-    <div class="col-sm-12 col-md-4 center">
-      <a href="http://www.mantistouch.org">
-        <img class="img-rounded" title="Dashboard Page on Smart Phone" alt="Dashboard Page" src="images/mt_dashboard_page.png" height="300" width="200" border="1" />
-      </a>
-      <div class="clearfix"></div>
-      <br>
-    </div>
-    <div class="col-sm-12 col-md-4 center">
-      <a href="http://www.mantistouch.org">
-        <img class="img-rounded" title="Issues Page on Smart Phone" alt="Issues Page" src="images/mt_issues_page.png" height="300" width="200" border="1" />
-      </a>
-      <div class="clearfix"></div>
-      <br>
+    <div class="col-sm-5 col-md-4">
+      <div class="tour-btn btn-default"> <a class="btn btn-warning" href="http://www.mantistouch.org">Learn more about MantisTouch</a></div>
     </div>
   </div>
 
-  <br>
-
   <div class="row">
-    <div class="col-sm-12 col-md-12 grey-box">
-      <div class="hero-block3">
-        <div class="row show-grid">
-          <div class="col-sm-9 col-md-9">
-            <div class="hero-content-3">
-              <h2>Get MantisTouch</h2>
-              <p>Enable your team to work and and collaborate while on the go. Get full access to MantisTouch code along
-                with detailed instrutions on how to install, configure and administer.</p>
-            </div>
-          </div>
-          <div class="col-sm-3 col-md-3">
-            <div class="tour-btn btn-default"> <a class="btn btn-warning" href="http://www.mantistouch.org">Learn More</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <a href="http://www.mantistouch.org">
+      <img class="img-rounded" title="Login Page on Smart Phone" alt="Login Page" src="images/mt_login_page.png" height="300" width="200" border="1" />
+    </a>
+    <a href="http://www.mantistouch.org">
+      <img class="img-rounded" title="Dashboard Page on Smart Phone" alt="Dashboard Page" src="images/mt_dashboard_page.png" height="300" width="200" border="1" />
+    </a>
+    <a href="http://www.mantistouch.org">
+      <img class="img-rounded" title="Issues Page on Smart Phone" alt="Issues Page" src="images/mt_issues_page.png" height="300" width="200" border="1" />
+    </a>
   </div>
 
 <?php
