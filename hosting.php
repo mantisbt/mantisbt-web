f773599eb68f00cd71fa2f5dd1bad7cf188c05f0
Victor Boctor <victor@mantishub.com>
2014-01-24 20:00:02 -0800

    Updated hosting page.

--- hosting.php
+++ hosting.php
@@ -13,7 +13,7 @@ include( "top.php" );
 
   <div class="row show-grid clear-both">
     <div class="col-sm-7 col-md-8">
-      <h1>MantisHub <small>| Beyond Generic Hosting</small></h1>
+      <h1>MantisHub <small>| MantisBT Served Fresh</small></h1>
 
       <p><span class="lead">MantisHub offers a hassle-free turn-key solution to MantisBT customers. Backed by a support
          team with years of experience building MantisBT.</span></p>
@@ -25,26 +25,19 @@ include( "top.php" );
         <h3>MantisHub for MantisBT is what GitHub is for Git.</3>
         <p class="autor">Victor Boctor</p>
       </blockquote>
-
-      With 30-day free trial, no credit card required, you can try MantisHub risk free. After the trial, you get to
-      decide whether to continue with MantisHub or seek a different solution. If you wish to continue using MantisHub,
-      you will be charged a small monthly fee based on the plan you select.
-
-      <div class="clear-both"></div>
-      <br>
-      <div class="text-divider3"></div>
     </div>
     <div class="col-sm-5 col-md-4">
       <div class="attention-stick stick pull-right" style="margin-top: 20px">
         <div style="font-size: 38px;"> <i class="icon-off"></i>
         </div>
         <br>
-        <h2>Try MantisHub</h2>
-        <h2>Now</h2>
+        <h2>Get Started</h2>
         <br>
         <a href="http://www.mantishub.com" type="button" class="ex btn btn-default btn-inverse btn-block">
           <i class="icon-arrow-right"></i> Start 30-day Free Trial
         </a>
+        <br />
+        no credit card required
       </div>
     </div>
   </div>
@@ -55,16 +48,22 @@ include( "top.php" );
       <br>
       <ul class="icons item-details">
         <li>
-          <i class="icon-ok"></i> Free 30-day trial - no credit card required
+          <i class="icon-ok"></i> Ability to get started in 1 minute with 30 day free trial.
+        </li>
+        <li>
+          <i class="icon-ok"></i> Focus on your business rather than worrying about backups, upgrades, and support.
         </li>
         <li>
           <i class="icon-ok"></i> Support provided by experienced team with extensive MantisBT experience
         </li>
         <li>
+          <i class="icon-ok"></i> Existing users are able to migrated their data.
+        </li>
+        <li>
           <i class="icon-ok"></i> Exclusive add-ons like <a href="http://www.mantistouch.org/">MantisTouch</a>
         </li>
         <li>
-          <i class="icon-ok"></i> No long term commitment - cancel anytime
+          <i class="icon-ok"></i> No long term commitment - cancel anytime and take your data with you.
         </li>
       </ul>
     </div>
