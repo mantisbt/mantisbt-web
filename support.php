1fa63c0a23a6ae9c09e908d549a78db475c903ce
Damien Regad <dregad@mantisbt.org>
2014-03-11 16:19:19 +0100

    Replace IRC channel #mantisbt-help by #mantisbt

--- support.php
+++ support.php
@@ -82,9 +82,9 @@ include( "top.php" );
       <div class="note-stick stick pull-left">
         <br>
         <p>Use IRC chat at</p>
-        <h2>#mantisbt-help</h2>
+        <h2>#mantisbt</h2>
         <br>
-        <a href="irc://irc.freenode.net/mantisbt-help" onclick="ga('send', 'event', 'Support', 'Go to IRC');" type="button" class="ex btn btn-group btn-default btn-inverse btn-block">
+        <a href="irc://irc.freenode.net/mantisbt" onclick="ga('send', 'event', 'Support', 'Go to IRC');" type="button" class="ex btn btn-group btn-default btn-inverse btn-block">
           Go to IRC
         </a>
         <a href="irclogs.php" onclick="ga('send', 'event', 'Support', 'Read channel archives');" type="button" class="ex btn btn-group btn-default btn-inverse btn-block">
