ac67ddb5e48c7ca3b669e2190631fb6bcd5df018
Damien Regad <dregad@mantisbt.org>
2014-06-18 02:21:24 +0200

    Coding guidelines

--- browser_check.php
+++ browser_check.php
@@ -6,89 +6,72 @@
  * http://php.net/function.get-browser.php#101125
  * @return array
  */
-function getBrowser()
-{
+function getBrowser() {
 	$u_agent = $_SERVER['HTTP_USER_AGENT'];
 	$bname = 'Unknown';
 	$platform = 'Unknown';
-	$version= "";
+	$version = '';
 
-	//First get the platform?
-	if (preg_match('/linux/i', $u_agent)) {
+	# First get the platform
+	if( preg_match( '/linux/i', $u_agent ) ) {
 		$platform = 'linux';
-	}
-	elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+	} elseif( preg_match( '/macintosh|mac os x/i', $u_agent ) ) {
 		$platform = 'mac';
-	}
-	elseif (preg_match('/windows|win32/i', $u_agent)) {
+	} elseif( preg_match( '/windows|win32/i', $u_agent ) ) {
 		$platform = 'windows';
 	}
 
-	// Next get the name of the useragent yes seperately and for good reason
-	if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent))
-	{
+	# Next get the name of the useragent yes seperately and for good reason
+	if( preg_match( '/MSIE/i', $u_agent ) && !preg_match( '/Opera/i', $u_agent ) ) {
 		$bname = 'Internet Explorer';
-		$ub = "MSIE";
-	}
-	elseif(preg_match('/Firefox/i',$u_agent))
-	{
+		$ub = 'MSIE';
+	} elseif( preg_match( '/Firefox/i', $u_agent ) ) {
 		$bname = 'Mozilla Firefox';
-		$ub = "Firefox";
-	}
-	elseif(preg_match('/Chrome/i',$u_agent))
-	{
+		$ub = 'Firefox';
+	} elseif( preg_match( '/Chrome/i', $u_agent ) ) {
 		$bname = 'Google Chrome';
-		$ub = "Chrome";
-	}
-	elseif(preg_match('/Safari/i',$u_agent))
-	{
+		$ub = 'Chrome';
+	} elseif( preg_match( '/Safari/i', $u_agent ) ) {
 		$bname = 'Apple Safari';
-		$ub = "Safari";
-	}
-	elseif(preg_match('/Opera/i',$u_agent))
-	{
-		$bname = 'Opera';
-		$ub = "Opera";
-	}
-	elseif(preg_match('/Netscape/i',$u_agent))
-	{
-		$bname = 'Netscape';
-		$ub = "Netscape";
+		$ub = 'Safari';
+	} elseif( preg_match( '/Opera/i', $u_agent ) ) {
+		$bname = $ub = 'Opera';
+	} elseif( preg_match( '/Netscape/i', $u_agent ) ) {
+		$bname = $ub = 'Netscape';
 	}
 
-	// finally get the correct version number
-	$known = array('Version', $ub, 'other');
-	$pattern = '#(?<browser>' . join('|', $known) .
-		')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-	if (!preg_match_all($pattern, $u_agent, $matches)) {
-		// we have no matching number just continue
+	# finally get the correct version number
+	$known = array( 'Version', $ub, 'other' );
+	$pattern = '#(?<browser>' . join( '|', $known ) . ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+	if( !preg_match_all( $pattern, $u_agent, $matches ) ) {
+		# we have no matching number just continue
 	}
 
-	// see how many we have
-	$i = count($matches['browser']);
-	if ($i != 1) {
-		//we will have two since we are not using 'other' argument yet
-		//see if version is before or after the name
-		if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+	# see how many we have
+	$i = count( $matches['browser'] );
+	if( $i != 1 ) {
+		# we will have two since we are not using 'other' argument yet
+		# see if version is before or after the name
+		if( strripos( $u_agent, 'Version' ) < strripos( $u_agent, $ub ) ) {
 			$version= $matches['version'][0];
+		} else {
+			$version = $matches['version'][1];
 		}
-		else {
-			$version= $matches['version'][1];
-		}
-	}
-	else {
-		$version= $matches['version'][0];
+	} else {
+		$version = $matches['version'][0];
 	}
 
-	// check if we have a number
-	if ($version==null || $version=="") {$version="?";}
+	# check if we have a number
+	if( $version == '' ) {
+		$version = '?';
+	}
 
 	return array(
 		'userAgent' => $u_agent,
 		'name'      => $bname,
 		'version'   => $version,
 		'platform'  => $platform,
-		'pattern'    => $pattern
+		'pattern'   => $pattern,
 	);
 }
 
@@ -96,12 +79,9 @@ function getBrowser()
  * Returns true if user's browser is not supported
  * @return bool
  */
-function is_unsupported_browser()
-{
+function is_unsupported_browser() {
 	$ua = getBrowser();
-	if ( 'Internet Explorer' == $ua['name'] &&
-		( '6.0' == $ua['version'] ) ) {
-
+	if( 'Internet Explorer' == $ua['name'] && '6.0' == $ua['version'] ) {
 		return true;
 	}
 
