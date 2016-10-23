aca62d70e7d87c1dcd164c3d9c79e156d7173037
Victor Boctor <victor@mantishub.net>
2016-10-23 09:48:20 -0700

    Remove ‘irc’

--- support.php
+++ support.php
@@ -70,18 +70,6 @@ include( "top.php" );
           Go to wiki
         </a>
       </div>
-      <div class="note-stick stick pull-left">
-        <br>
-        <p>Use IRC chat at</p>
-        <h1>#mantisbt</h1>
-        <a href="irc://irc.freenode.net/mantisbt" onclick="ga('send', 'event', 'Support', 'Go to IRC');" type="button" class="ex btn btn-group btn-default btn-inverse btn-block">
-          Go to IRC
-        </a>
-        <a href="irclogs.php" onclick="ga('send', 'event', 'Support', 'Read channel archives');" type="button" class="ex btn btn-group btn-default btn-inverse btn-block">
-          Read channel archives
-        </a>
-      </div>
-
     </div>
   </div>
 
